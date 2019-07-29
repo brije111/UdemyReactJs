4f2d945c88bba22bbd7eae1bf351f280c6ffb894
Android32018 <Androidhcl@123>
2019-07-29 18:31:50 +0530

    from 134

--- src/blog_project/actions/index.js
+++ src/blog_project/actions/index.js
@@ -1,4 +1,5 @@
 import JsonPlaceholder from "../apis/JsonPlaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => async dispatch => {
   const response = await JsonPlaceholder.get("/posts");
@@ -15,3 +16,12 @@ export const fetchUser = (id) => async dispatch => {
       payload: response.data
     });
   };
+
+//   export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch)=>{
+//     const response = await JsonPlaceholder.get(`/users/${id}`);
+//     dispatch({
+//       type: "FETCH_USER",
+//       payload: response.data
+//     });
+//   });
